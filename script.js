eb89c3e914ed6d31eceb053a8f70a19675bf22fc
LeoZheng279 <leozheng279@gmail.com>
2025-02-03 14:34:35 +0800

    added mouse detect algorithms

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 const container = document.querySelector(".container");
+let isMouseDown = false;
+document.addEventListener("mousedown", (mouse) => {
+    if (mouse.button === 0) isMouseDown = true;
+})
+document.addEventListener("mouseup", (mouse) => {
+    if (mouse.button === 0) isMouseDown = false;
+})
 
 for(let j = 0; j<16; j++)
 {
@@ -8,6 +15,12 @@ for(let j = 0; j<16; j++)
         {
             const rect = document.createElement("div");
             rect.classList.add("rect");
+            rect.addEventListener("mouseenter", () =>{
+                if (isMouseDown) rect.style.backgroundColor = "black";
+            })
+            rect.addEventListener("mousedown", () =>{
+                rect.style.backgroundColor = "black";
+            })
             line.appendChild(rect);
         }
     container.appendChild(line);
