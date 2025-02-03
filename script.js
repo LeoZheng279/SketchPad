7c54ea2ad545aadcc095882b7846ce2f7a0308f4
LeoZheng279 <leozheng279@gmail.com>
2025-02-03 16:21:41 +0800

    finished color picker

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+let penColor = "black";
 let isMouseDown = false;
 document.addEventListener("mousedown", (mouse) => {
     if (mouse.button === 0) isMouseDown = true;
@@ -6,6 +7,12 @@ document.addEventListener("mousedown", (mouse) => {
 document.addEventListener("mouseup", (mouse) => {
     if (mouse.button === 0) isMouseDown = false;
 })
+
+document.addEventListener('coloris:pick', event => {
+    console.log('New color', event.detail.color);
+    penColor = event.detail.color;
+  });
+
 
 for(let j = 0; j<16; j++)
 {
@@ -16,12 +23,13 @@ for(let j = 0; j<16; j++)
             const rect = document.createElement("div");
             rect.classList.add("rect");
             rect.addEventListener("mouseenter", () =>{
-                if (isMouseDown) rect.style.backgroundColor = "black";
+                if (isMouseDown) rect.style.backgroundColor = penColor;
             })
             rect.addEventListener("mousedown", () =>{
-                rect.style.backgroundColor = "black";
+                rect.style.backgroundColor = penColor;
             })
             line.appendChild(rect);
         }
     container.appendChild(line);
 }
+
